5be256993bfbefb48f2a1ce07aa983d2dc3528e7
pavel <pavelpoley88@gmail.com>
2018-03-21 14:48:56 +0200

    added app module example

--- app/src/main/java/pavelpoley/com/socialutilshelper/MainActivity.java
+++ app/src/main/java/pavelpoley/com/socialutilshelper/MainActivity.java
@@ -1,44 +1,73 @@
 package pavelpoley.com.socialutilshelper;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.view.View;
-import android.widget.Button;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import pavelpoley.com.androidsocialutils.SocialUtils;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
-    Button btShare,btRate,btSendEmail;
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        btShare = findViewById(R.id.bt_share);
-        btRate = findViewById(R.id.bt_rate);
-        btSendEmail = findViewById(R.id.bt_send_email);
 
-        btShare.setOnClickListener(this);
-        btRate.setOnClickListener(this);
-        btSendEmail.setOnClickListener(this);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()){
-            case R.id.bt_share:
-                SocialUtils.shareApp(this);
-                break;
-
-            case R.id.bt_rate:
-                SocialUtils.rateApp(this);
-                break;
-
-            case R.id.bt_send_email:
-                SocialUtils.sendEmail(this,"pavelpoley88@gmail.com","Feedback");
-                break;
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
         }
+    }
+
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_rate) {
+            SocialUtils.rateApp(this);
+
+        } else if (id == R.id.nav_share) {
+            SocialUtils.shareApp(this);
+
+        } else if (id == R.id.nav_send) {
+            SocialUtils.sendEmail(this,"pavelpoley88@gmail.com","Feedback");
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
